0363c20d12c315ed33b934073cfe8f8e45c0903f
Bernard Pauler <paul25bern@gmail.com>
2020-03-19 17:09:02 +0800

    first commit initial Commit

---
+++ printstatement.py
@@ -0,0 +1,11 @@
+UserName = 'bernard'
+fullName = 'bernard finkle'
+
+print(UserName)
+print(fullName)
+
+altName = UserName
+IncorrectName = 'fullName'
+newName = UserName = fullName
+print(newName)
+print(UserName)
